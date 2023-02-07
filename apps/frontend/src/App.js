674f20a941da305a2d818dc674652d9a712adefe
Carl Bergenhem <cbergenhem@gmail.com>
2023-02-07 12:11:27 -0500

    Add shared site-info package

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Import the shared function into the `create-react-app` application
+import { getSiteInfo } from 'site-info';
+
+export default function App() {
+  //Define a variable to get the values
+  let siteInfo = getSiteInfo();
   return (
     <div className="App">
+      {/*Output the site title and subtitle to the screen*/}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{siteInfo.title}</h1>
+        <p>{siteInfo.subtitle}</p>
       </header>
     </div>
   );
 }
-
-export default App;
