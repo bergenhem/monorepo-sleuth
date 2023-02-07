be866e67d71405e73703395374228a9192113fb5
Carl Bergenhem <cbergenhem@gmail.com>
2023-02-07 12:26:44 -0500

    Remove shared resouces

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 
-// Import the shared function into the `create-react-app` application
-import { getSiteInfo } from 'site-info';
-
 export default function App() {
   //Define a variable to get the values
-  let siteInfo = getSiteInfo();
   return (
     <div className="App">
       {/*Output the site title and subtitle to the screen*/}
       <header className="App-header">
-        <h1>{siteInfo.title}</h1>
-        <p>{siteInfo.subtitle}</p>
+        <h1>Monorepo Test</h1>
       </header>
     </div>
   );
